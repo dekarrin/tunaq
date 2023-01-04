539db37444b01269057fa61ce33fb9ff35fda672
dekarrin <dekarrin.irl@gmail.com>
2023-01-04 13:30:03 -0600

    make world.toml be actual output of script

--- scripts/json2tqw.py
+++ scripts/json2tqw.py
@@ -8,8 +8,10 @@ import textwrap
 
 def main():
     if len(sys.argv) < 2:
-        print("give name of file to convert as first arg", file=sys.stderr)
+        print("give name of file to convert as first arg and file to write output to as second arg", file=sys.stderr)
         exit(1)
+    if len(sys.argv) < 3:
+        print("give name of file to write output to as second arg")
 
     filename = str(sys.argv[1])
     with open(filename, 'r') as fp:
@@ -19,144 +21,149 @@ def main():
             print("malormed JSON in file: " + str(e))
             exit(2)
 
-    # print preamble
-    print('format = "TUNA"')
-    print('type = "DATA"')
-    print('')
+    output_file = str(sys.argv[2])
+    with open(output_file, 'w') as fp:
+        # print preamble
+        print('format = "TUNA"', file=fp)
+        print('type = "DATA"', file=fp)
+        print('', file=fp)
 
-    # print world (pretty much just the start key)
-    print('[world]')
-    if 'start' in json_data:
-        print('start = "' + str(json_data['start']) + '"')
-    else:
-        print("WARN: input is missing key 'start'; world.start will be undefined in output", file=sys.stderr)
-        print('start = ""')
-    print()
-    print()
-
-    # print each room
-    for r in json_data.get('rooms', []):
-        print('# ' + r.get('label', '').lower())
-        print('[[rooms]]')
-        print('label = "' + r.get('label', '') + '"')
-        print('name = "' + r.get('name', '') + '"')
-        print("description = '''")
-        for l in textwrap.wrap(r.get('description', ''), 120):
-            print(l)
-        print("'''")
-        print()
-        
-        for eg in r.get('exits', []):
-            print('[[rooms.exits]]')
-            print('aliases = ' + format_inline_text_list(eg.get('aliases', [])))
-            print('destLabel = "' + eg.get('destLabel', '') + '"')
-            print('description = "' + eg.get('description', '') + '"')
-            print("travelMessage = '''")
-            for l in textwrap.wrap(eg.get('travelMessage', ''), 120):
-                print(l)
-            print("'''")
-            print()
-
-        for it in r.get('items', []):
-            print('[[rooms.items]]')
-            print('label = "' + it.get('label', '') + "'")
-            print('aliases = ' + format_inline_text_list(it.get('aliases', [])))
-            print('name = "' + it.get('name', '') + "'")
-            print("description = '''")
-            for l in textwrap.wrap(eg.get('description', ''), 120):
-                print(l)
-            print("'''")
-            print()
-        
-        print()
-
-    # print each pronoun set
-    pronoun_sets = json_data.get('pronouns', {})
-    for k in pronoun_sets:
-        ps = pronoun_sets[k]
-        print('[pronouns."' + str(k) + '"]')
-        print('nominative = "' + ps.get('nominative', '') + '"')
-        print('objective = "' + ps.get('objective', '') + '"')
-        print('possessive = "' + ps.get('possessive', '') + '"')
-        print('determiner = "' + ps.get('determiner', '') + '"')
-        print('reflexive = "' + ps.get('reflexive', '') + '"')
-        print()
-    if len(pronoun_sets) > 0:
-        print()
-
-    # print each npc
-    for npc in json_data.get('npcs', []):
-        print('[[npcs]]')
-        print('label = "' + npc.get('label', '') + '"')
-        print('aliases = ' + format_multiline_text_list(npc.get('aliases', [])))
-        print('name = "' + npc.get('name', '') + '"')
-        print('start = "' + npc.get('start', '') + '"')
-
-        pn = npc.get('pronouns', "")
-        pronouns_after = False
-
-        if isinstance(pn, str):
-            print('pronouns = "' + str(pn) + "'")
+        # print world (pretty much just the start key)
+        print('[world]', file=fp)
+        if 'start' in json_data:
+            print('start = "' + str(json_data['start']) + '"', file=fp)
         else:
-            pronouns_after = True
+            print("WARN: input is missing key 'start'; world.start will be undefined in output", file=sys.stderr)
+            print('start = ""', file=fp)
+        print('', file=fp)
+        print('', file=fp)
 
-        print("description = '''")
-        for l in textwrap.wrap(npc.get('description', ''), 120):
-            print(l)
-        print("'''")
-        print()
+        # print each room
+        for r in json_data.get('rooms', []):
+            print('# ' + r.get('label', '').lower(), file=fp)
+            print('[[rooms]]', file=fp)
+            print('label = "' + r.get('label', '') + '"', file=fp)
+            print('name = "' + r.get('name', '') + '"', file=fp)
+            print("description = '''", file=fp)
+            for l in textwrap.wrap(r.get('description', ''), 120):
+                print(l, file=fp)
+            print("'''", file=fp)
+            print('', file=fp)
+            
+            for eg in r.get('exits', []):
+                print('[[rooms.exits]]', file=fp)
+                print('aliases = ' + format_inline_text_list(eg.get('aliases', [])), file=fp)
+                print('destLabel = "' + eg.get('destLabel', '') + '"', file=fp)
+                print('description = "' + eg.get('description', '') + '"', file=fp)
+                print("travelMessage = '''", file=fp)
+                for l in textwrap.wrap(eg.get('travelMessage', ''), 120):
+                    print(l, file=fp)
+                print("'''", file=fp)
+                print('', file=fp)
 
-        if pronouns_after:
-            print("[npcs.pronouns]")
-            print('nominative = "' + pn.get('nominative', '') + '"')
-            print('objective = "' + pn.get('objective', '') + '"')
-            print('possessive = "' + pn.get('possessive', '') + '"')
-            print('determiner = "' + pn.get('determiner', '') + '"')
-            print('reflexive = "' + pn.get('reflexive', '') + '"')
-            print()
+            for it in r.get('items', []):
+                print('[[rooms.items]]', file=fp)
+                print('label = "' + it.get('label', '') + '"', file=fp)
+                print('aliases = ' + format_inline_text_list(it.get('aliases', [])), file=fp)
+                print('name = "' + it.get('name', '') + '"', file=fp)
+                print("description = '''", file=fp)
+                for l in textwrap.wrap(eg.get('description', ''), 120):
+                    print(l, file=fp)
+                print("'''", file=fp)
+                print('', file=fp)
+            
+            print('', file=fp)
 
-        mv = npc.get('movement', {})
-        if mv is not None:
-            act = mv.get('action', 'STATIC')
-            print('[npcs.movement]')
-            print('action = "' + act + '"')
-            if 'path' in mv:
-                print('path = ' + format_multiline_text_list(mv['path']))
-            if 'allowedRooms' in mv:
-                print('allowedRooms = ' + format_multiline_text_list(mv['allowedRooms']))
-            if 'forbiddenRooms' in mv:
-                print('forbiddenRooms = ' + format_multiline_text_list(mv['forbiddenRooms']))
-            print()
+        # print each pronoun set
+        pronoun_sets = json_data.get('pronouns', {})
+        for k in pronoun_sets:
+            ps = pronoun_sets[k]
+            print('[pronouns."' + str(k) + '"]', file=fp)
+            print('nominative = "' + ps.get('nominative', '') + '"', file=fp)
+            print('objective = "' + ps.get('objective', '') + '"', file=fp)
+            print('possessive = "' + ps.get('possessive', '') + '"', file=fp)
+            print('determiner = "' + ps.get('determiner', '') + '"', file=fp)
+            print('reflexive = "' + ps.get('reflexive', '') + '"', file=fp)
+            print('', file=fp)
+        if len(pronoun_sets) > 0:
+            print('', file=fp)
 
-        # TODO: rename this field to 'dialogs' or somefin ending in plural to
-        # stay consistent at least with rule of 'ending in s, add more brackets!'
-        for dia in npc.get('dialog', []):
-            if isinstance(dia, str):
-                dia = {
-                    'content': dia,
-                }
-            print('[[npcs.dialog]]')
-            if 'label' in dia:
-                print('label = "' + dia['label'] + '"')
-            if 'action' in dia:
-                print('action = "' + dia['action'] + '"')
-            if 'content' in dia:
-                print('content = "' + dia['content'] + '"')
-            if 'response' in dia:
-                print('response = "' + dia['response'] + '"')
-            if 'choices' in dia:
-                print('choices = ' + format_multiline_text_list(dia['choices'], format_inline_text_list))
-            print()
+        # print each npc
+        for npc in json_data.get('npcs', []):
+            print('[[npcs]]', file=fp)
+            print('label = "' + npc.get('label', '') + '"', file=fp)
+            print('aliases = ' + format_multiline_text_list(npc.get('aliases', [])), file=fp)
+            print('name = "' + npc.get('name', '') + '"', file=fp)
+            print('start = "' + npc.get('start', '') + '"', file=fp)
+
+            pn = npc.get('pronouns', "")
+            pronouns_after = False
+
+            if isinstance(pn, str):
+                print('pronouns = "' + str(pn) + '"', file=fp)
+            else:
+                pronouns_after = True
+
+            print("description = '''", file=fp)
+            for l in textwrap.wrap(npc.get('description', ''), 120):
+                print(l, file=fp)
+            print("'''", file=fp)
+            print('', file=fp)
+
+            if pronouns_after:
+                print("[npcs.pronouns]", file=fp)
+                print('nominative = "' + pn.get('nominative', '') + '"', file=fp)
+                print('objective = "' + pn.get('objective', '') + '"', file=fp)
+                print('possessive = "' + pn.get('possessive', '') + '"', file=fp)
+                print('determiner = "' + pn.get('determiner', '') + '"', file=fp)
+                print('reflexive = "' + pn.get('reflexive', '') + '"', file=fp)
+                print('', file=fp)
+
+            mv = npc.get('movement', {})
+            if mv is not None:
+                act = mv.get('action', 'STATIC')
+                print('[npcs.movement]', file=fp)
+                print('action = "' + act + '"', file=fp)
+                if 'path' in mv:
+                    print('path = ' + format_multiline_text_list(mv['path']), file=fp)
+                if 'allowedRooms' in mv:
+                    print('allowedRooms = ' + format_multiline_text_list(mv['allowedRooms']), file=fp)
+                if 'forbiddenRooms' in mv:
+                    print('forbiddenRooms = ' + format_multiline_text_list(mv['forbiddenRooms']), file=fp)
+                print('', file=fp)
+
+            # TODO: rename this field to 'dialogs' or somefin ending in plural to
+            # stay consistent at least with rule of 'ending in s, add more brackets!'
+            for dia in npc.get('dialog', []):
+                if isinstance(dia, str):
+                    dia = {
+                        'content': dia,
+                    }
+                print('[[npcs.dialog]]', file=fp)
+                if 'label' in dia:
+                    print('label = "' + dia['label'] + '"', file=fp)
+                if 'action' in dia:
+                    print('action = "' + dia['action'] + '"', file=fp)
+                if 'content' in dia:
+                    print('content = "' + dia['content'] + '"', file=fp)
+                if 'response' in dia:
+                    print('response = "' + dia['response'] + '"', file=fp)
+                if 'choices' in dia:
+                    print('choices = ' + format_multiline_text_list(dia['choices'], format_inline_text_list), file=fp)
+                print('', file=fp)
 
 
-def format_multiline_text_list(text_list, op=str):
+def format_multiline_text_list(text_list, op=None):
+    if op is None:
+        op = lambda s: '"' + str(s) + '"'
+
     out = "["
     if len(text_list) > 0:
         out += "\n"
     for t in text_list:
-        out += "\t\""
+        out += "\t"
         out += op(t)
-        out += "\",\n"
+        out += ",\n"
     out += ']'
     return out
 
@@ -167,9 +174,9 @@ def format_inline_text_list(text_list):
     for t in text_list:
         out += "\""
         out += str(t)
-        out += "\","
+        out += "\""
         if itemNum + 1 < len(text_list):
-            out += " "
+            out += ", "
 
         itemNum += 1
     out += ']'
