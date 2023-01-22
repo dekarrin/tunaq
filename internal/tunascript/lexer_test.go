e211d0d1e66eafe870237d71d22071757988b25c
dekarrin <dekarrin.irl@gmail.com>
2023-01-21 21:36:51 -0600

    Added sample ide file

--- internal/tunascript/lexer_test.go
+++ internal/tunascript/lexer_test.go
@@ -1,6 +1,7 @@
 package tunascript
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -13,11 +14,21 @@ func Test_Lex_tokenClassSequence(t *testing.T) {
 		expect    []tokenClass
 		expectErr bool
 	}{
-		{
-			name:   "blank string",
-			input:  "",
-			expect: []tokenClass{tsEndOfText},
-		},
+		{name: "blank string", input: "", expect: []tokenClass{tsEndOfText}},
+		{name: "1 digit number", input: "1", expect: []tokenClass{tsNumber, tsEndOfText}},
+		{name: "2 digit number", input: "39", expect: []tokenClass{tsNumber, tsEndOfText}},
+		{name: "3 digit number", input: "026", expect: []tokenClass{tsNumber, tsEndOfText}},
+		{name: "4 digit number", input: "4578", expect: []tokenClass{tsNumber, tsEndOfText}},
+		{name: "2 numbers", input: "3284 1384", expect: []tokenClass{tsNumber, tsNumber, tsEndOfText}},
+		{name: "we dont do decimals, thats a string", input: "13.4", expect: []tokenClass{tsUnquotedString, tsEndOfText}},
+		{name: "bool on", input: "on", expect: []tokenClass{tsBool, tsEndOfText}},
+		{name: "bool off", input: "OFF", expect: []tokenClass{tsBool, tsEndOfText}},
+		{name: "bool true", input: "tRuE", expect: []tokenClass{tsBool, tsEndOfText}},
+		{name: "bool false", input: "False", expect: []tokenClass{tsBool, tsEndOfText}},
+		{name: "bool yes", input: "yeS", expect: []tokenClass{tsBool, tsEndOfText}},
+		{name: "bool no", input: "no", expect: []tokenClass{tsBool, tsEndOfText}},
+		{name: "some string", input: "fdksalfjaskldfj", expect: []tokenClass{tsUnquotedString, tsEndOfText}},
+		{name: "a few random values", input: "no yes test string 3 eight", expect: []tokenClass{tsBool, tsBool, tsUnquotedString, tsUnquotedString, tsNumber, tsUnquotedString, tsEndOfText}},
 	}
 
 	for _, tc := range testCases {
@@ -35,7 +46,16 @@ func Test_Lex_tokenClassSequence(t *testing.T) {
 				actual[i] = actualStream.tokens[i].class
 			}
 
-			assert.Equal(tc.expect, actual)
+			expectStrings := make([]string, len(tc.expect))
+			for i := range tc.expect {
+				expectStrings[i] = tc.expect[i].id
+			}
+			actualStrings := make([]string, len(actual))
+			for i := range actual {
+				actualStrings[i] = actual[i].id
+			}
+
+			assert.Equal(strings.Join(expectStrings, " "), strings.Join(actualStrings, " "))
 		})
 	}
 }
