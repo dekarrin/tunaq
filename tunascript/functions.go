a0d0661c7a554ccb367fa9ff9dc8d8e79027d311
Deka Jello <dekarrin.irl@gmail.com>
2023-06-13 14:55:58 -0500

    add tags to tracked list of tagsets to close #70

--- tunascript/functions.go
+++ tunascript/functions.go
@@ -6,26 +6,26 @@ import (
 	"github.com/dekarrin/tunaq/tunascript/syntax"
 )
 
-type funcImpl func(args []syntax.Value) syntax.Value
+type funcImpl func(args []Value) Value
 
 type funcInfo struct {
 	def  syntax.Function
 	call funcImpl
 }
 
-func unaryImpl(fname string, impl func(v syntax.Value) syntax.Value) funcInfo {
+func unaryImpl(fname string, impl func(v Value) Value) funcInfo {
 	return funcInfo{
 		def: syntax.BuiltInFunctions[fname],
-		call: func(args []syntax.Value) syntax.Value {
+		call: func(args []Value) Value {
 			return impl(args[0])
 		},
 	}
 }
 
-func binaryImpl(fname string, impl func(v syntax.Value, v2 syntax.Value) syntax.Value) funcInfo {
+func binaryImpl(fname string, impl func(v Value, v2 Value) Value) funcInfo {
 	return funcInfo{
 		def: syntax.BuiltInFunctions[fname],
-		call: func(args []syntax.Value) syntax.Value {
+		call: func(args []Value) Value {
 			return impl(args[0], args[1])
 		},
 	}
@@ -35,21 +35,21 @@ func binaryImpl(fname string, impl func(v syntax.Value, v2 syntax.Value) syntax.
 func (interp *Interpreter) initFuncs() {
 	interp.fn = map[string]funcInfo{}
 
-	interp.fn["ADD"] = binaryImpl("ADD", syntax.Value.Add)
-	interp.fn["SUB"] = binaryImpl("SUB", syntax.Value.Subtract)
-	interp.fn["MULT"] = binaryImpl("MULT", syntax.Value.Multiply)
-	interp.fn["DIV"] = binaryImpl("DIV", syntax.Value.Divide)
-	interp.fn["NEG"] = unaryImpl("NEG", syntax.Value.Negate)
-	interp.fn["OR"] = binaryImpl("OR", syntax.Value.Or)
-	interp.fn["AND"] = binaryImpl("AND", syntax.Value.And)
-	interp.fn["NOT"] = unaryImpl("NOT", syntax.Value.Not)
-	interp.fn["FLAG_ENABLED"] = unaryImpl("FLAG_ENABLED", syntax.Value.CastToBool)
-	interp.fn["FLAG_DISABLED"] = unaryImpl("FLAG_DISABLED", func(v syntax.Value) syntax.Value {
+	interp.fn["ADD"] = binaryImpl("ADD", Value.Add)
+	interp.fn["SUB"] = binaryImpl("SUB", Value.Subtract)
+	interp.fn["MULT"] = binaryImpl("MULT", Value.Multiply)
+	interp.fn["DIV"] = binaryImpl("DIV", Value.Divide)
+	interp.fn["NEG"] = unaryImpl("NEG", Value.Negate)
+	interp.fn["OR"] = binaryImpl("OR", Value.Or)
+	interp.fn["AND"] = binaryImpl("AND", Value.And)
+	interp.fn["NOT"] = unaryImpl("NOT", Value.Not)
+	interp.fn["FLAG_ENABLED"] = unaryImpl("FLAG_ENABLED", Value.CastToBool)
+	interp.fn["FLAG_DISABLED"] = unaryImpl("FLAG_DISABLED", func(v Value) Value {
 		return v.CastToBool().Negate()
 	})
-	interp.fn["FLAG_IS"] = binaryImpl("FLAG_IS", syntax.Value.EqualTo)
-	interp.fn["FLAG_LESS_THAN"] = binaryImpl("FLAG_LESS_THAN", syntax.Value.LessThan)
-	interp.fn["FLAG_GREATER_THAN"] = binaryImpl("FLAG_GREATER_THAN", syntax.Value.GreaterThan)
+	interp.fn["FLAG_IS"] = binaryImpl("FLAG_IS", Value.EqualTo)
+	interp.fn["FLAG_LESS_THAN"] = binaryImpl("FLAG_LESS_THAN", Value.LessThan)
+	interp.fn["FLAG_GREATER_THAN"] = binaryImpl("FLAG_GREATER_THAN", Value.GreaterThan)
 	interp.fn["ENABLE"] = unaryImpl("ENABLE", interp.enable)
 	interp.fn["DISABLE"] = unaryImpl("DISABLE", interp.disable)
 	interp.fn["TOGGLE"] = unaryImpl("TOGGLE", interp.toggle)
@@ -61,7 +61,7 @@ func (interp *Interpreter) initFuncs() {
 	// and the two variable-arity functions
 	interp.fn["INC"] = funcInfo{
 		def: syntax.BuiltInFunctions["INC"],
-		call: func(args []syntax.Value) syntax.Value {
+		call: func(args []Value) Value {
 			if len(args) > 1 {
 				return interp.inc(args[0], args[1])
 			}
@@ -70,7 +70,7 @@ func (interp *Interpreter) initFuncs() {
 	}
 	interp.fn["DEC"] = funcInfo{
 		def: syntax.BuiltInFunctions["DEC"],
-		call: func(args []syntax.Value) syntax.Value {
+		call: func(args []Value) Value {
 			if len(args) > 1 {
 				return interp.dec(args[0], args[1])
 			}
@@ -79,31 +79,31 @@ func (interp *Interpreter) initFuncs() {
 	}
 }
 
-func (interp *Interpreter) inInven(v syntax.Value) syntax.Value {
+func (interp *Interpreter) inInven(v Value) Value {
 	itemLabelName := strings.ToUpper(v.String())
 
 	return syntax.ValueOf(interp.Target.InInventory(itemLabelName))
 }
 
-func (interp *Interpreter) move(target, dest syntax.Value) syntax.Value {
+func (interp *Interpreter) move(target, dest Value) Value {
 	targetStr := strings.ToUpper(target.String())
 	destStr := strings.ToUpper(dest.String())
 
 	return syntax.ValueOf(interp.Target.Move(targetStr, destStr))
 }
 
-func (interp *Interpreter) output(msg syntax.Value) syntax.Value {
+func (interp *Interpreter) output(msg Value) Value {
 	return syntax.ValueOf(interp.Target.Output(msg.String()))
 }
 
-func (interp *Interpreter) set(name syntax.Value, value syntax.Value) syntax.Value {
+func (interp *Interpreter) set(name Value, value Value) Value {
 	flagName := strings.ToUpper(name.String())
 
 	interp.flags[flagName] = value
 	return value
 }
 
-func (interp *Interpreter) enable(v syntax.Value) syntax.Value {
+func (interp *Interpreter) enable(v Value) Value {
 	flagName := strings.ToUpper(v.String())
 	newVal := syntax.ValueOf(true)
 
@@ -111,7 +111,7 @@ func (interp *Interpreter) enable(v syntax.Value) syntax.Value {
 	return newVal
 }
 
-func (interp *Interpreter) disable(v syntax.Value) syntax.Value {
+func (interp *Interpreter) disable(v Value) Value {
 	flagName := strings.ToUpper(v.String())
 	newVal := syntax.ValueOf(false)
 
@@ -119,7 +119,7 @@ func (interp *Interpreter) disable(v syntax.Value) syntax.Value {
 	return newVal
 }
 
-func (interp *Interpreter) toggle(v syntax.Value) syntax.Value {
+func (interp *Interpreter) toggle(v Value) Value {
 	flagName := strings.ToUpper(v.String())
 	curVal := interp.flags[flagName]
 	newVal := curVal.CastToBool().Not()
@@ -128,7 +128,7 @@ func (interp *Interpreter) toggle(v syntax.Value) syntax.Value {
 	return newVal
 }
 
-func (interp *Interpreter) inc(name syntax.Value, amount syntax.Value) syntax.Value {
+func (interp *Interpreter) inc(name Value, amount Value) Value {
 	flagName := strings.ToUpper(name.String())
 
 	curVal := interp.flags[flagName]
@@ -138,7 +138,7 @@ func (interp *Interpreter) inc(name syntax.Value, amount syntax.Value) syntax.Va
 	return newVal
 }
 
-func (interp *Interpreter) dec(name syntax.Value, amount syntax.Value) syntax.Value {
+func (interp *Interpreter) dec(name Value, amount Value) Value {
 	flagName := strings.ToUpper(name.String())
 
 	curVal := interp.flags[flagName]
